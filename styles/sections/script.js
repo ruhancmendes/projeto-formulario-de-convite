1688f5466bc2dac375e994a45285de3aaa9f3523
ruhancmendes <ruhancmendes@gmail.com>
2024-12-20 18:22:44 -0300

    add switch button

---
+++ styles/sections/script.js
@@ -0,0 +1,10 @@
+    const checkbox = document.getElementById('toggle-dark-mode');
+    const modeText = document.getElementById('mode-text');
+
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        modeText.textContent = 'Claro';
+      } else {
+        modeText.textContent = 'Escuro';
+      }
+    });
